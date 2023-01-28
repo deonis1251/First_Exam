e26e2b9eac49111a8fa3f13ad9d9abf8521992db
Denis <denis_gulbin@mail.ru>
2023-01-28 19:19:24 +0300

    Program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите элементы массива через пробел: ");
+string[] AddArr = AddArray();
+string[] OutArr = new string[AddArr.Length];
+LimitLenghtArray(AddArr, OutArr);
+PrintArray(OutArr);
+
+//Вводим элементы массива, который будем перебирать 
+string[] AddArray()
+{
+    return Console.ReadLine().Split(" ");
+}
+
+//Метод выбора элементов, которые короче 3 элементов, из массива
+void LimitLenghtArray(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[j] = array1[i];
+        j++;
+        }
+    }
+}
+
+//Печать массива
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
